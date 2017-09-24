7426e81a9e027f725e5c31bdfcc17400711aca8f
Tesserex <tesserex@gmail.com>
2017-09-23 21:08:16 -0700

    add quoting of posts

--- fuglbrennamvc/Areas/Forum/Content/js/forum.js
+++ fuglbrennamvc/Areas/Forum/Content/js/forum.js
@@ -10,6 +10,7 @@
         ],
         toolbar: 'insert | undo redo |  styleselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
         content_css: [
+            '/Areas/Forum/Content/css/forum.css'
         ]
     });
 
@@ -20,5 +21,19 @@
 
         var localFull = m.local().format("LLLL");
         $(e).attr('title', localFull);
+    });
+
+    var quoteTemplate = $($("#forum-quote-template").html());
+
+    $("body").on("click", "a.post-quote", function () {
+        var post = $(this).closest(".forum-posts__post");
+        var memberName = post.data("member");
+        var postDate = moment.utc(post.data("date")).local().format("LLLL");
+        var postContent = post.find(".post__main .content").html();
+        var quote = $("<div></div>").append(quoteTemplate.clone());
+        quote.find(".forum-quote__header__name").html(memberName);
+        quote.find(".forum-quote__header__date").html(postDate);
+        quote.find(".forum-quote__content").html(postContent);
+        tinymce.activeEditor.execCommand('mceInsertContent', false, quote.html());
     });
 });
